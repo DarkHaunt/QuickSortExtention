2f0bf7a5622ed5f5552a981458a909b17e3e5f9e
Dark Haunt <yaroslavkyzyk@gmail.com>
2023-04-29 00:09:04 +0300

    Added Simple Float Randomizer + Test Launch

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
-﻿internal class Program
+﻿using QuickSort;
+
+internal class Program
 {
+    private const int CountOfElementsToCreate = 30;
+
     private static void Main(string[] args)
-    {
+    {   
+        Console.WriteLine("--- Start ---\n");
+        Console.WriteLine($"--- Count of elements - {CountOfElementsToCreate}\n");
+
+        var randomCollectionGenerator = new RandomCollectionGenerator();
+
+        foreach (var randomFloat in randomCollectionGenerator.GetRandomFloatPositiveCollection(CountOfElementsToCreate))
+            Console.WriteLine($"{randomFloat}\n");
+
+        Console.ReadLine();
     }
 }
---
+++ RandomCollectionGenerator.cs
@@ -0,0 +1,19 @@
+﻿
+namespace QuickSort
+{
+    internal class RandomCollectionGenerator
+    {
+        private const int MaxValue = 100000;
+        private static readonly Random Randomizer = new Random(TimeOnly.MaxValue.Second);
+
+
+        public RandomCollectionGenerator() { }
+
+
+        public IEnumerable<float> GetRandomFloatPositiveCollection(int count)
+        {
+            for (int i = 0; i < count; i++)
+                yield return Randomizer.Next(MaxValue) * Randomizer.NextSingle();
+        }
+    }
+}
